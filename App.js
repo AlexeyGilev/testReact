adb39a512e38a28f53a7f5e270a819f72432faa0
Alexey <alexeygilev94@gmail.com>
2019-05-07 14:30:11 +0500

    FlatList

--- App.js
+++ App.js
@@ -1,65 +1,120 @@
 import React from 'react';
-import { Text, ScrollView } from 'react-native';
+import { Text, ScrollView, View, FlatList, StyleSheet } from 'react-native';
 import Picker from 'react-native-picker';
-data = [
-    {
-        a: [
-             {
-             'первый набор': ['aaa', 'bbb', 'ccc', 'ddd']
-             },
-             {
-             'второй набор': [1, 2, 3, 4]
-             },
-             {
-             'третий набор': [5, 6, 7, 8]
-             }
-        ]
-    },
-    {
-        b: [
-            {
-                b1: [11, 22, 33, 44]
-            },
-            {
-                b2: [55, 66, 77, 88]
-            },
-            {
-                b3: [99, 1010, 1111, 1212]
-            }
-        ]
-    },
-    {
-        c: [
-            {
-                c1: ['a', 'b', 'c']
-            },
-            {
-                c2: ['aa', 'bb', 'cc']
-            },
-            {
-                c3: ['aaa', 'bbb', 'ccc']
-            }
-        ]
-    },
-]
-
 export default class App extends React.Component{
     constructor(props){
         super(props);
-        Picker.init({
-                    pickerData: data,
-                    selectedValue: [59]
-                    });
-
     }
     render(){
         Picker.show();
         return(
-               <ScrollView>
-                <Text></Text>
-               </ScrollView>
+          <View style={styles.container}>
+              <FlatList
+              data = {
+                             [
+                                 {
+                                     "name": "Proxima Midnight",
+                                     "email": "proxima@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Ebony Maw",
+                                     "email": "ebony@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Black Dwarf",
+                                     "email": "dwarf@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Mad Titan",
+                                     "email": "thanos@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Supergiant",
+                                     "email": "supergiant@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Loki",
+                                     "email": "loki@appdividend.com"
+                                 },
+                                 {
+                                     "name": "corvus",
+                                     "email": "corvus@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Proxima Midnight",
+                                     "email": "proxima1@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Ebony Maw",
+                                     "email": "ebony1@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Black Dwarf",
+                                     "email": "dwarf1@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Mad Titan",
+                                     "email": "thanos1@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Supergiant",
+                                     "email": "supergiant1@appdividend.com"
+                                 },
+                                 {
+                                     "name": "Loki",
+                                     "email": "loki1@appdividend.com"
+                                 },
+                                 {
+                                     "name": "corvus",
+                                     "email": "corvus1@appdividend.com"
+                                 }
+                             ]
+                     }
+              showsVerticalScrollIndicator={false}
+              renderItem={({item}) =>
+              <View>
+                <Text style={styles.name}>Название: {item.name}</Text>
+                <Text style={styles.email}>Email: {item.email}</Text>
+                <Text style={styles.email}>Тема: тестовая</Text>
+                <Text style={styles.email}>Содержание: без содержания</Text>
+              </View>
+              }
+              keyExtractor={item => item.email}
+            />
+          </View>
         )
     }
 
-
 }
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        marginTop: 10,
+        marginLeft: 20,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        backgroundColor: '#F5FCFF',
+      },
+      h2text: {
+        marginTop: 10,
+        fontFamily: 'Helvetica',
+        fontSize: 36,
+        fontWeight: 'bold',
+      },
+      flatview: {
+        justifyContent: 'center',
+        paddingTop: 30,
+        borderRadius: 2,
+        borderColor: 'black',
+      },
+      name: {
+        fontFamily: 'Verdana',
+        fontSize: 20,
+        color: 'black'
+      },
+      email: {
+        color: 'gray'
+      }
+
+    });
